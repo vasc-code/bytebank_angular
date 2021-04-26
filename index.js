e36f1a8fb4320ea137672dbfe75c9e4580ada3a5
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-26 17:21:46 -0300

    aula 2

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+class ContaCorrente{
+    agencia;
+    saldo;
 
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }        
+    }
+}
+const cliente1 = new Cliente();
 cliente1.nome = "Rafael";
 cliente1.cpf = 11122233344;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const contaCorrenteRafael = new ContaCorrente()
+contaCorrenteRafael.agencia = 1001;
+contaCorrenteRafael.saldo = 0;
+
+console.log(contaCorrenteRafael.saldo);
+contaCorrenteRafael.saldo = 100;
+console.log(contaCorrenteRafael.saldo);
+contaCorrenteRafael.sacar(50);
+console.log(contaCorrenteRafael.saldo);
+
+
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 11111111100;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1,cliente2);
+const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.agencia = 1001;
+contaCorrenteAlice.saldo = 0;
+
+console.log(cliente1);
+console.log(cliente2);
