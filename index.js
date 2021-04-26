59fd9941b9a209d5eb9c454699088ac8d92591e7
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-26 16:46:55 -0300

    inicio do projeto

---
+++ index.js
@@ -0,0 +1,11 @@
+const cliente1Nome = "Rafael";
+const cliente1Cpf = 12345678900;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2Cpf = 88888888888;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
