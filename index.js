8ccefe84c0b5f8bd4b190b7f78bfe9edc36a421e
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-26 16:56:49 -0300

    criando classe Cliente

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const cliente1Nome = "Rafael";
-const cliente1Cpf = 12345678900;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
 
-const cliente2Nome = "Alice";
-const cliente2Cpf = 88888888888;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+cliente1.nome = "Rafael";
+cliente1.cpf = 11122233344;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
 
-console.log(cliente1Nome);
+console.log(cliente1.nome);
