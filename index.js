b9d658032dbb3f66a3fa55b4fdb86eeb32146c9b
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-26 17:02:39 -0300

    finalizando classe Cliente

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ class Cliente{
 }
 
 const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
 cliente1.nome = "Rafael";
 cliente1.cpf = 11122233344;
 cliente1.agencia = 1001;
 cliente1.saldo = 0;
 
-console.log(cliente1.nome);
+cliente2.nome = "Alice";
+cliente2.cpf = 11111111100;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente1,cliente2);
