71bee9893d66ee6df8a95605b3df998306151646
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-29 09:39:08 -0300

    dando inicio ao projeto

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 
 const cliente1 = new Cliente("Rafael",11122233344);
-
 const cliente2 = new Cliente("Alice",11111111100);
 
 const contaCorrenteRafael = new ContaCorrente(1001,cliente1);
+contaCorrenteRafael.depositar(500);
+contaCorrenteRafael.sacar(100);
 
 const contaCorrenteAlice = new ContaCorrente(101,cliente2);
 
-let valor = 200
-contaCorrenteRafael.depositar(500);
+let valor = 200;
 contaCorrenteRafael.transferir(valor, contaCorrenteAlice);
 
 console.log(contaCorrenteRafael);
